f04c1945291c61692b7e81b1a24e98a28cbf5375
gramar <marcus.grass@gmail.com>
2021-11-22 23:36:44 +0100

    Fuzz comparison

--- fuzz/fuzz_targets/headers.rs
+++ fuzz/fuzz_targets/headers.rs
@@ -1,7 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use parse_range_headers::{RangeHeaderParserBuilder, ParsedRangeHeader};
 use regex::Regex;
+use headers::{HeaderValue};
+use headers::Header;
 
 lazy_static::lazy_static! {
     static ref STANDARD_RANGE: Regex = Regex::new("^bytes=((\\d+-\\d+,\\s)|(\\d+-,\\s)|(-\\d+,\\s))*((\\d+-\\d+)|(\\d+-)|(-\\d+))+$").unwrap();
@@ -12,13 +13,10 @@ fuzz_target!(|data: &[u8]| {
         if STANDARD_RANGE.is_match(s) {
             return;
         }
-
-        let parser = RangeHeaderParserBuilder::new()
-            .with_unit_label("bytes")
-            .build();
-
-        let parsed = parser.parse(s);
-
-        assert!(parsed == ParsedRangeHeader::Unsatisfiable || parsed == ParsedRangeHeader::Malformed, "Range was not rejected input=`{}` range={:?}", s, parsed);
+        if let Ok(header) = HeaderValue::from_str(s) {
+            if let Ok(_h) = headers::Range::decode(&mut vec![header].iter()) {
+                //assert!(n.is_none(), "Range was not rejected by headers, input=`{}` range={:?}", s, n);
+            }
+        }
     }
 });
